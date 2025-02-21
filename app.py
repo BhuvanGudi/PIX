44b2c30abf320ae08201949ad067da277b537825
Bhuvan Gudimetla <gudimetlabhuvan@gmail.com>
2025-02-21 00:55:41 -0700

    working button and input-box(EditText)

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ ctk.set_appearance_mode("dark")
 prompt = ctk.CTkEntry(app, height=40, width=512, font=("Arial", 20), text_color="black", fg_color="white")
 prompt.place(x=10, y=10)
 
-lmain = ctk.CTkLabel(height=512, width=512)
+lmain = ctk.CTkLabel(app, height=512, width=512)
 lmain.place(x=10, y=110)
 
-trigger = ctk.CTkButton(app, height=40, width=512, font=("Arial", 20), text_color="white", fg_color="blue")
+trigger = ctk.CTkButton(app, height=40, width=120, font=("Arial", 20), text_color="white", fg_color="blue")
 trigger.place(x=206, y=60)
 
 app.mainloop()
